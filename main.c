210ccec6aa722146b064465ce7873ba676d3c637
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-09-23 18:34:06 -0300

    main.c of stage 3

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 /*
-Função principal para realização da análise sintática.
+Função principal para realização da E3.
 
-Este arquivo será posterioremente substituído, não acrescente nada.
+Este arquivo não pode ser modificado.
 */
 #include <stdio.h>
 #include "parser.tab.h" //arquivo gerado com bison -d parser.y
 extern int yylex_destroy(void);
 
+void *arvore = NULL;
+void libera (void *arvore);
+void exporta (void *arvore);
+
 int main (int argc, char **argv)
 {
     int ret = yyparse();
+    exporta (arvore);
+    libera(arvore);
+    arvore = NULL;
     yylex_destroy();
     return ret;
 }
