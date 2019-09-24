4b1b181b706a4ec26d9b8a08c0ec38789f8aa971
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-09-24 17:22:05 -0300

    Fix compilation errors

--- Makefile
+++ Makefile
@@ -1,13 +1,14 @@
 all: parser.y scanner.l main.c
 	bison -d parser.y --report-file=report.out -r all
 	flex scanner.l
-	gcc -c lex.yy.c parser.tab.c
-	gcc -o etapa3 lex.yy.o parser.tab.o main.c -ll
+	gcc -c lex.yy.c parser.tab.c tree.c
+	gcc -o etapa3 lex.yy.o parser.tab.o tree.o main.c -ll
 
 test: test.txt etapa3
 	cat test.txt | ./etapa3
 
 clean:
+	rm -f *.o
 	rm -f parser.tab.*
 	rm -f report.out
 	rm -f lex.yy.*
--- tree.c
+++ tree.c
@@ -1,5 +1,6 @@
 #include "tree.h"
 #include "lexical.h"
+#include <stdlib.h>
 
 Node* create_node(Lexeme* value) {
     Node* node = (Node*) malloc(sizeof(Node));
@@ -16,7 +17,7 @@ void add_node(Node* parent, Node* child) {
     parent->n_children = parent->n_children + 1;
 
     if(parent->n_children == 1) parent->children = (Node**) malloc(sizeof(Node*));
-    else parent->children = (Node**) realloc(sizeof(Node*) * parent->n_children);
+    else parent->children = (Node**) realloc(parent->children, sizeof(Node*) * parent->n_children);
 
     int index = parent->n_children - 1;
     parent->children[index] = child;
@@ -30,11 +31,39 @@ void add_lexeme(Node* parent, Lexeme* value) {  // alias for add_node(parent, cr
     add_node(parent, child);
 }
 
+void unlink_child(Node* parent, const int index) {
+    for(int i = index; i < parent->n_children - 1; i++)
+        parent->children[index] = parent->children[index + 1];
+
+    parent->n_children = parent->n_children - 1;
+    parent->children = (Node**) realloc(parent->children, sizeof(Node*) * parent->n_children);
+}
+
+void unlink_node(Node* node) {
+    unlink_child(node->parent, node->index);
+}
+
+void destroy_value(Lexeme* lexeme) {
+    switch (lexeme->token_type) {
+        case TK_PR:
+        case TK_OC:
+        case TK_ID:
+        case TK_LT:
+            if(lexeme->literal_type == LT_STRING || lexeme->literal_type == LT_NAL) {
+                free(lexeme->token_value.string);
+                lexeme->token_value.string = NULL;
+            }
+    }
+
+    free(lexeme);
+    lexeme = NULL;
+}
+
 void destroy_node(Node* node) {
     unlink_node(node);
 
     for(int i = 0; i < node->n_children; i++)
-        remove_node(node->children[i]);
+        destroy_node(node->children[i]);
 
     free(node->children);
     node->children = NULL;
@@ -44,31 +73,6 @@ void destroy_node(Node* node) {
 
     free(node);
     node = NULL;
-}
-
-void unlink_node(Node* node) {
-    unlink_child(node->parent, node->index);
-}
-
-void unlink_child(Node* parent, const int index) {
-    for(int i = index; i < parent->n_children - 1; i++)
-        parent->children[index] = parent->children[index + 1];
-
-    parent->n_children = parent->n_children - 1;
-    parent->children = (Node**) realloc(sizeof(Node*) * parent->n_children);
-}
-
-void destroy_value(Lexeme* lexeme) {
-    switch (lexeme->token_type) {
-        case TK_PR, TK_OC, TK_ID, TK_LT:
-            if(lexeme->literal_type == LT_STRING || lexeme->literal_type == LT_NAL) {
-                free(lexeme->token_value.string);
-                lexeme->token_value.string = NULL;
-            }
-    }
-
-    free(lexeme);
-    lexeme = NULL;
 }
 
 void update_node(Node* node, Node* new_node) {
