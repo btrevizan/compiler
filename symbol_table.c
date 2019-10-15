ffc78e54873392cf67c18ba3b0d85d0d258302f3
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-10-15 17:16:12 -0300

    Hash table implementation

---
+++ symbol_table.c
@@ -0,0 +1,207 @@
+#include "symbol_table.h"
+#include "lexical.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int hash(const char* key, const int prime_number, const int length) {
+    long hashcode = 0;
+    const int key_len = strlen(key);
+
+    for (int i = 0; i < key_len; i++) {
+        hashcode += (long) pow(prime_number, key_len - (i + 1)) * key[i];
+        hashcode = hashcode % length;
+    }
+
+    return (int) hashcode;
+}
+
+int get_hash(const char* key, const int length, const int attempt) {
+    const int hash_a = hash(key, PRIME_1, length);
+    const int hash_b = hash(key, PRIME_2, length);
+    return (hash_a + (attempt * (hash_b + 1))) % length;
+}
+
+int get_nature_size(int nature) {
+    switch (nature) {
+        case NATUREZA_LITERAL_INT:
+            return sizeof(int);
+
+        case NATUREZA_LITERAL_FLOAT:
+            return sizeof(float);
+
+        case NATUREZA_LITERAL_CHAR:
+            return sizeof(char);
+
+        case NATUREZA_LITERAL_STRING:
+            return sizeof(char*);
+
+        case NATUREZA_LITERAL_BOOL:
+            return sizeof(int);
+
+        case NATUREZA_IDENTIFICADOR:
+            return sizeof(char*);
+
+        default:
+            return -1;
+    }
+}
+
+int is_prime(int x) {
+    if(x % 2 == 0) return 0;
+
+    for(int i = 3; i <= sqrt((float) x); i++)
+        if(x % i == 0) return 0;
+
+    return 1;
+}
+
+int next_prime(int x) {
+    while(is_prime(x) == 0)
+        x++;
+
+    return x;
+}
+
+void resize(Table* table, int new_size) {
+    if(new_size < TABLE_LENGTH) return;
+
+    table->base_size = new_size;
+    table->size = next_prime(table->base_size);
+    table->entries = realloc(table->entries, table->size * sizeof(Entry*));
+
+    memset((table->entries + table->count), 0, table->size * sizeof(Entry*) - table->count);
+}
+
+Table* create_table() {
+    return create_sized_table(TABLE_LENGTH);
+}
+
+Table* create_sized_table(int size) {
+    Table* table = malloc(sizeof(Table));
+
+    table->base_size = size;
+    table->size = next_prime(table->base_size);
+    table->count = 0;
+    table->entries = calloc((size_t) table->size, sizeof(Entry*));
+
+    return table;
+}
+
+Entry* create_entry(const char* key, Symbol* value) {
+    Entry* entry = malloc(sizeof(Entry));
+
+    entry->key = strdup(key);
+    entry->value = value;
+
+    return entry;
+}
+
+Symbol* create_symbol(int line_number, int nature, int args_number, Symbol** args, Lexeme* lexeme) {
+    Symbol* symbol = malloc(sizeof(Symbol));
+
+    symbol->line_number = line_number;
+    symbol->nature = nature;
+    symbol->size = get_nature_size(symbol->nature);
+    symbol->args_number = args_number;
+    symbol->args = args;
+    symbol->lexeme = lexeme;
+
+    return symbol;
+}
+
+void add_entry(Table* table, Entry* entry) {
+    if((float) table->count / table->size > 0.7)
+        resize(table, table->base_size * 2);
+
+    int i = get_hash(entry->key, table->size, 0);
+    Entry* current_entry = table->entries[i];
+
+    int attempt = 1;
+    while(current_entry != NULL && current_entry != &DELETED_ENTRY) {
+        i = get_hash(entry->key, table->size, attempt);
+        current_entry = table->entries[i];
+        attempt++;
+    }
+
+    table->entries[i] = entry;
+    table->count++;
+}
+
+void add_symbol(Table* table, const char* key, Symbol* value) {
+    Entry* entry = create_entry(key, value);
+    add_entry(table, entry);
+}
+
+Symbol* get_entry(Table* table, const char* key) {
+    int i = get_hash(key, table->size, 0);
+    Entry* current_entry = table->entries[i];
+
+    int attempt = 1;
+    while(current_entry != NULL) {
+        if(current_entry != &DELETED_ENTRY && strcmp(current_entry->key, key) == 0)
+            return current_entry->value;
+
+        i = get_hash(key, table->size, attempt);
+        current_entry = table->entries[i];
+        attempt++;
+    }
+
+    return NULL;
+}
+
+void remove_entry(Table* table, const char* key) {
+    int i = get_hash(key, table->size, 0);
+    Entry* current_entry = table->entries[i];
+
+    int attempt = 1;
+    while(current_entry != NULL) {
+        if(current_entry != &DELETED_ENTRY && strcmp(current_entry->key, key) == 0) {
+            delete_entry(current_entry);
+            table->entries[i] = &DELETED_ENTRY;
+            break;
+        }
+
+        i = get_hash(key, table->size, attempt);
+        current_entry = table->entries[i];
+        attempt++;
+    }
+
+    table->count--;
+}
+
+void delete_symbol(Symbol* symbol) {
+    for(int i = 0; i < symbol->args_number; i++) {
+        Symbol* symbol = symbol->args[i];
+        if(symbol != NULL) delete_symbol(symbol);
+    }
+
+    delete_lexeme(symbol->lexeme);
+
+    free(symbol->args);
+    free(symbol);
+
+    symbol = NULL;
+}
+
+void delete_entry(Entry* entry) {
+    free(entry->key);
+
+    delete_symbol(entry->value);
+    free(entry);
+
+    entry = NULL;
+}
+
+void delete_table(Table* table) {
+    for(int i = 0; i < table->size; i++) {
+        Entry* entry = table->entries[i];
+        if(entry != NULL) delete_entry(entry);
+    }
+
+    free(table->entries);
+    free(table);
+
+    table = NULL;
+}
---
+++ symbol_table.h
@@ -0,0 +1,57 @@
+#ifndef COMPILER_SYMBOL_TABLE_H
+#define COMPILER_SYMBOL_TABLE_H
+
+// Based on https://github.com/jamesroutley/write-a-hash-table
+
+#include "lexical.h"
+#include <stdlib.h>
+
+#define TABLE_LENGTH 20
+#define PRIME_1 131
+#define PRIME_2 137
+
+#define NATUREZA_LITERAL_INT        1
+#define NATUREZA_LITERAL_FLOAT      2
+#define NATUREZA_LITERAL_CHAR       3
+#define NATUREZA_LITERAL_STRING     4
+#define NATUREZA_LITERAL_BOOL       5
+#define NATUREZA_IDENTIFICADOR      6
+
+typedef struct symbol {
+    int line_number;
+    int nature;
+    int size;
+    int args_number;
+    struct symbol** args;
+    Lexeme* lexeme;
+} Symbol;
+
+typedef struct entry {
+    char* key;
+    Symbol* value;
+} Entry;
+
+typedef struct table {
+    int size;
+    int count;
+    int base_size;
+    Entry** entries;
+} Table;
+
+Entry DELETED_ENTRY = {NULL, NULL};
+
+Table* create_table();
+Table* create_sized_table(int size);
+Entry* create_entry(const char* key, Symbol* value);
+Symbol* create_symbol(int line_number, int nature, int args_number, Symbol** args, Lexeme* lexeme);
+
+void add_entry(Table* table, Entry* entry);
+void add_symbol(Table* table, const char* key, Symbol* value);
+Symbol* get_entry(Table* table, const char* key);
+void remove_entry(Table* table, const char* key);
+
+void delete_symbol(Symbol* symbol);
+void delete_entry(Entry* entry);
+void delete_table(Table* table);
+
+#endif
