6c06f934ba5f009cde097c58e1a8bb7ac20a139e
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-10-17 20:21:58 -0300

    Test file written by Scarlat7

---
+++ test.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+ETAPA=
+VALGRIND=false
+VERBOSE=false
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-e | --etapa ) 	shift
+						ETAPA=$1
+						;;
+		-m | --valgrind )		VALGRIND=true
+						;;
+		-v | --verbose ) 		VERBOSE=true
+						;;
+		* )				exit 1
+	esac
+	shift
+done
+
+if [ -z "$ETAPA" ]
+then
+	echo "Please provide the parameter (-e/--etapa), an integer between 1 and 3"
+	exit 1
+fi
+
+rm test_result.txt
+
+TEST_DIR="tests/e${ETAPA}/*"
+
+for testfile in $TEST_DIR
+do
+	echo "************ TEST $testfile ************" >> test_result.txt
+	if [ "$VERBOSE" = true ]; then
+		# Print the contents of the test file
+		cat $testfile >> test_result.txt
+	fi
+	if [ "$VALGRIND" = true ]; then
+		# Executes with valgrind
+		cat $testfile | valgrind ./etapa$ETAPA  >> test_result.txt 2>&1
+	else
+		# Executes without valgrind
+		cat $testfile | ./etapa$ETAPA >> test_result.txt 2>&1
+	fi
+done
