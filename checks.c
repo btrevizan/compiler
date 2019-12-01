698730f10c9a41dd2b4ffd99820af7a02b533465
Scarlat7 <nataliarampon@hotmail.com>
2019-12-01 18:53:28 -0500

    Fix issue with argument check and expressions

--- checks.c
+++ checks.c
@@ -189,8 +189,13 @@ void check_args(Stack *scope, Node *id, Node *args) {
 
         // Go to next argument and to next parameter
         params_list = params_list->next;
-        if(args_list->n_children != 0)
+        if(args_list->n_children != 0){
+            if ((args_list->value->token_type == TK_SC  || args_list->value->token_type == TK_OC ) && args_list->n_children < 3){
+                args_list = NULL;
+                break;
+            }
             args_list = args_list->children[args_list->n_children - 1];
+        }
         else args_list = NULL;
         seen_count++;
     }
